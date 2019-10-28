f6b24738740d16d56f93e7ea9179a7c95043b0d6
Udayudi <udaygam@outlook.com>
2019-10-28 12:16:56 +0530

    Updated Swagger Config

--- src/main/java/com/jemtron/swaggerexample/config/SwaggerConfig.java
+++ src/main/java/com/jemtron/swaggerexample/config/SwaggerConfig.java
@@ -9,36 +9,26 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public Docket productApi(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.jemtron.swaggerexample"))
-                .paths(regex("/rest.*"))
-                .build()
-                .apiInfo(metaInfo());
-    }
+        @Bean
+        public Docket productApi() {
+                return new Docket(DocumentationType.SWAGGER_2).select()
+                                .apis(RequestHandlerSelectors.basePackage("com.jemtron.swaggerexample"))
+                                .paths(regex("/rest.*")).build().apiInfo(metaInfo());
+        }
 
-    private ApiInfo metaInfo() {
-        ApiInfo apiInfo = new ApiInfo(
-                "Spring Boot Swagger Exaple API",
-                "Spring Boot Swagger Expale API for Exaple",
-                "1.0",
-                "Terms of Service",
-                new Contact("Udaykirna A", "http://jemtron.com",
-                        "udaygam@outlook.com"),
-                "Apache License Version 2.0",
-                "https://www.apache.ogg/licesen.html"
-        );
+        private ApiInfo metaInfo() {
+                ApiInfo apiInfo = new ApiInfo("Spring Boot Swagger Exaple API",
+                                "Spring Boot Swagger Expale API for Exaple", "1.0", "Terms of Service",
+                                new Contact("Udaykirna A", "https://www.linkedin.com/in/udaykiran-a-661b1663",
+                                                "udaygam@outlook.com"),
+                                "Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0");
                 return apiInfo;
-    }
-
+        }
 
 }
